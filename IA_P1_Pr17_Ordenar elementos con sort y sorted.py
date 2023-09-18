607f6882fa017c4e6a477671455af4df021639c0
JoseCruz <145302830+21110350J@users.noreply.github.com>
2023-09-17 20:56:31 -0600

    Add files via upload

---
+++ IA_P1_Pr17_Ordenar elementos con sort y sorted.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 20:51:03 2023
+
+@author: Jose Cruz TP
+"""
+
+#En esta practica vamos a realizar en nuestra lista un orden alfabetico descendente
+#con nuestra funcion sort
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón',
+'lila', 'negro', 'rosa', 'blanco', 'naranja']
+colores.sort(reverse=True)
+print(colores)
