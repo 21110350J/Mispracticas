74d1f56c5f17b66b7fe75ab0119099c789deea40
JoseCruz <145302830+21110350J@users.noreply.github.com>
2023-09-17 23:14:50 -0600

    Add files via upload

---
+++ IA_P1_Pr30_La función range en bucles for.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 23:07:55 2023
+
+@author: Jose Cruz TP
+"""
+
+#En esta practica vamos a realizar con  un ciclo for bucles que vaya desde el valor 7 hasta el valor 700 en saltos de 100.
+
+for x in range(7, 700, 100):
+	print(x)
