131f74b43448660519f83e6e7645475244efac1a
JoseCruz <145302830+21110350J@users.noreply.github.com>
2023-09-18 00:17:33 -0600

    Add files via upload

---
+++ IA_P1_Pr39_Qué es self de Python.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 18 00:15:39 2023
+
+@author: Jose Cruz TP
+"""
+
+class Usuario:
+	def __init__(self, nombre, apellidos)
+		self.nombre = nombre
+	self.apellidos = apellidos
+
+	def imprime_datos(self):
+		print('Nombre:', self.nombre, '\nApellidos:', self.apellidos)
+
+usuario001 = Usuario('Enrique', 'Barros Fernández')
+
+usuario002 = Usuario('Javier', 'Gomila Reyes')
+
+usuario002.nombre = 'Jacinto'
+
+usuario002.imprime_datos()
