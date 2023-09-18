421f7fc6ef20675aa3827270297ca4f66c69e772
JoseCruz <145302830+21110350J@users.noreply.github.com>
2023-09-17 20:41:09 -0600

    Add files via upload

---
+++ IA_P1_Pr15_Insertar elementos con append.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 20:30:14 2023
+
+@author: Jose Cruz TP
+"""
+
+#Aqui vamos a insertar nuevos elementos a la lista que tengamos
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+#Añade los colores 'fuxia' y 'celeste' a la lista.
+colores.append("fuxia")
+colores.append("celeste")
+#Mostramos la lista con los nuevos 
+print(colores)
