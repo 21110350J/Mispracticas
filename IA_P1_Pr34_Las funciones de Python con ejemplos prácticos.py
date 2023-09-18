e30062079c721c4a521adc9ce13cde67c236b073
JoseCruz <145302830+21110350J@users.noreply.github.com>
2023-09-17 23:51:00 -0600

    Add files via upload

---
+++ IA_P1_Pr34_Las funciones de Python con ejemplos prácticos.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 23:47:25 2023
+
+@author: Jose Cruz TP
+"""
+
+#En este codigo lo que vamos a realizar es la creacion para que haga una suma
+def suma(numero1, numero2):
+	print(numero1 + numero2)
+#Valores de las variables 
+suma(10, 20)
+suma(20, 30)
+suma(50000, 7000)
