d379d99e1aac3fbaaa882518b52950c8e83f6c11
JoseCruz <145302830+21110350J@users.noreply.github.com>
2023-09-17 22:23:44 -0600

    Add files via upload

---
+++ IA_P1_Pr26_Formas de abreviar condicionales en Python.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 22:16:26 2023
+
+@author: Jose Cruz TP
+"""
+
+#Aqui vamos a ver como abreviar condicionales en python 
+x = 300
+y = 200
+z = 100
+
+if x < y or z > y:
+    print("se cumplen las condiciones ")
+else:
+    print("no se cumplen ninguna condicion")
