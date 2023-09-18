e114968ce0776d3a90770077660c7659bb4b7334
JoseCruz <145302830+21110350J@users.noreply.github.com>
2023-09-17 22:37:04 -0600

    Add files via upload

---
+++ IA_P1_Pr27_Qué es y cómo funciona el bucle while.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 22:26:18 2023
+
+@author: Jose Cruz TP
+"""
+
+#Aqui vamos a utlizar lo que es el while para hacer incrementos y decrementos
+x = 0
+ 
+while x <= 15:
+     print(x)
+     x +=5
+     
+y = 0
+
+while y >= -100:
+    print(y)
+    y -= 20 
+    
+z = 10
+
+while z >= 0:
+	print('El valor de z es: ', z)
+	z -= 1
+    
