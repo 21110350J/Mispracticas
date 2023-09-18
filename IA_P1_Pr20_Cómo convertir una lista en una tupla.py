3f6bccff29d3e9af0b2858daf22e619ca30267f5
JoseCruz <145302830+21110350J@users.noreply.github.com>
2023-09-17 21:12:25 -0600

    Add files via upload

---
+++ IA_P1_Pr20_Cómo convertir una lista en una tupla.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 21:09:27 2023
+
+@author: Jose Cruz TP
+"""
+
+#En este ejercicio vamos a transformar una lista en una tupla 
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+tupla = tuple(colores)
+#y para verificar el cambio usamos type para saber si se realizo
+print(type(tupla))
