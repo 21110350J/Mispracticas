0c88672b9b29b47442e2c3e4b5e37823329e1086
JoseCruz <145302830+21110350J@users.noreply.github.com>
2023-09-17 21:19:00 -0600

    Add files via upload

---
+++ IA_P1_Pr21_El condicional IF de Python.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 21:14:37 2023
+
+@author: Jose Cruz TP
+"""
+
+#Aqui vamos a utilizar el condicional if para reafirmar ciertos casos
+#Caso_1
+num1 = 15
+num2 = 20
+
+if num1 < num2:
+	print('Se ejecuta el caso 1.')
+#Caso_2
+num3 = 1450
+num4 = 60
+
+if num3 > num4:
+	print('Se ejecuta el caso 2.')    
+    
+#Caso_3
+num1 = 1450
+num2 = 60
+
+if num1 == num2:
+	print('Se ejecuta el caso 3.')     
