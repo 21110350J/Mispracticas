5fea098f8cc17cd3c27b3a4a169f58b32600e49b
JoseCruz <145302830+21110350J@users.noreply.github.com>
2023-09-17 22:11:34 -0600

    Add files via upload

---
+++ IA_P1_Pr25_Se pueden utilizar múltiples IF en Python sin elif.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 22:09:35 2023
+
+@author: Jose Cruz TP
+"""
+#Aqui solo vemos si podemos usar varios if sin elif 
+entrada = int(input('Introduce un número del 1 al 4:\n'))
+
+if entrada == 1:
+    print('Has seleccionado la opción número 1.')
+if entrada == 2:
+    print('Has seleccionado la opción número 2.')
+if entrada == 3:
+    print('Has seleccionado la opción número 3.')
+if entrada == 4:
+    print('Has seleccionado la opción número 4.')
