6f525379a291f0984b1b4d30811d99d772791b94
JoseCruz <145302830+21110350J@users.noreply.github.com>
2023-09-17 21:27:22 -0600

    Add files via upload

---
+++ IA_P1_Pr22_Cómo funciona if con else en Python.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 21:19:47 2023
+
+@author: Jose Cruz TP
+"""
+
+#Aqui lo que vamos a utilizar es el else para cuando no aplique el if 
+#Se correra nuestro else
+
+color = "rojo"
+
+if color != "rojo":
+  print("El color es rojo.")
+else:
+  print("El color no es rojo.")
