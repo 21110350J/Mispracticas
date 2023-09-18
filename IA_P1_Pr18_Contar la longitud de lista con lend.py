df027194d33c37b33b9f3644c5b457a0da0705bc
JoseCruz <145302830+21110350J@users.noreply.github.com>
2023-09-17 21:01:05 -0600

    Add files via upload

---
+++ IA_P1_Pr18_Contar la longitud de lista con lend.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 20:57:44 2023
+
+@author: Jose Cruz TP
+"""
+#Aqui lo que vamos hacer es dar a conocer cuantos elementos tenemos en la lista 
+colores = ['rojo', 'azul', 'verde', 'amarillo']
+#Mostramos cuantos tenemos
+print(len(colores))
+
