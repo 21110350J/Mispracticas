9ab25275aac442748ec3282142b51509b92c1a07
JoseCruz <145302830+21110350J@users.noreply.github.com>
2023-09-17 21:08:05 -0600

    Add files via upload

---
+++ IA_P1_Pr19_Crear y manejar Tuplas.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 21:04:10 2023
+
+@author: Jose Cruz TP
+"""
+
+#Aqui usaremos tupla que y vamos a mostrar posicion y una operacion 
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+print(colores[1])
+#Ahora con numeros
+numeros = (10, 1, 5, 11)
+operacion = numeros[0] + numeros[2] + numeros[3] - numeros[1]
+#Y mostramos el resultado
+print(operacion)
