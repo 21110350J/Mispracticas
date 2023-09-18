ecb67275f9ecf180f207ef06878bf10c95ed96e5
JoseCruz <145302830+21110350J@users.noreply.github.com>
2023-09-17 23:03:44 -0600

    Add files via upload

---
+++ IA_P1_Pr29_Ejemplos con el bucle for.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 22:55:52 2023
+
+@author: Jose Cruz TP
+"""
+#En este ejercicio vamos a utilizar el ciclo for para mostrar el contenido 
+colores = ('rojo','azul','verde','amarillo')
+#Nuestra funcion for
+for x in colores:
+    print('El color es: ' + x + '.')
+    
